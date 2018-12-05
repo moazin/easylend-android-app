fe3cdc54fb90cd23fddac53c4517d60a814d440f
Moazin Khatri <moazinkhatri@gmail.com>
2018-12-05 17:35:06 +0500

    app bar replaced with custom one, color set properly and menu btton added

--- app/src/main/java/com/example/moazin/easylend/MainActivity.java
+++ app/src/main/java/com/example/moazin/easylend/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.moazin.easylend;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -16,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setting up the toolbar as the top bar
+        Toolbar customToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(customToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
         // TODO: Make Landscape available in this app
 
@@ -36,5 +46,15 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

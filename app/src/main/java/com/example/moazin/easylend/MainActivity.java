9fbd91e9b1f1ef45e75405d3dabf3b66ee561eee
Moazin Khatri <moazinkhatri@gmail.com>
2018-12-05 15:19:07 +0500

    some design modifications to make Huzaifa happy!

--- app/src/main/java/com/example/moazin/easylend/MainActivity.java
+++ app/src/main/java/com/example/moazin/easylend/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.moazin.easylend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent launchLoginActivitiy = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(launchLoginActivitiy);
     }
 }

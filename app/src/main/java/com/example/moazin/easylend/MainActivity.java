1dce32e7834989d0ad0691d38c8587aef5784d50
Moazin Khatri <moazinkhatri@gmail.com>
2018-12-05 16:09:01 +0500

    navigation drawer half layout done, tool bar button and custom toolbar remaining

--- app/src/main/java/com/example/moazin/easylend/MainActivity.java
+++ app/src/main/java/com/example/moazin/easylend/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.moazin.easylend;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent launchLoginActivitiy = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(launchLoginActivitiy);
+        // TODO: Make Landscape available in this app
+
+        // TODO: Commenting this out for testing purpose
+        // Intent launchLoginActivitiy = new Intent(MainActivity.this, LoginActivity.class);
+        // startActivity(launchLoginActivitiy);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         // TODO: if login is not done, redirect to login activity
-        Intent go_back_to_login = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(go_back_to_login);
+//        Intent go_back_to_login = new Intent(MainActivity.this, LoginActivity.class);
+//        startActivity(go_back_to_login);
         super.onResume();
     }
 }

c1b6c0decebdda4915cdf1d3b2ec5b47385ef387
Moazin Khatri <moazinkhatri@gmail.com>
2018-12-06 15:12:27 +0500

    implemented recycler view on exchange fragment that takes a json array (for now internally created) and displays stuff

---
+++ app/src/main/java/com/example/moazin/easylend/ExchangeAdapter.java
@@ -0,0 +1,70 @@
+package com.example.moazin.easylend;
+
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ExchangeAdapter extends RecyclerView.Adapter<ExchangeAdapter.MyViewHolder> {
+    private JSONArray mDataset;
+
+    // constructor for this adapter
+    public ExchangeAdapter(JSONArray dataset){
+        mDataset = dataset;
+    }
+
+    // a count returning function
+    @Override
+    public int getItemCount() {
+        return mDataset.length();
+    }
+
+    // create a view holder object for our list items, each item is a constraint layout object
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView personName;
+        public TextView personExchange;
+        public MyViewHolder(ConstraintLayout constraintLayout){
+            super(constraintLayout);
+            personName = constraintLayout.findViewById(R.id.person_name);
+            personExchange = constraintLayout.findViewById(R.id.person_exchange);
+        }
+    }
+
+    // a view holder creator that inflates layouts
+    @NonNull
+    @Override
+    public ExchangeAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ConstraintLayout constraintLayout = (ConstraintLayout) LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.exchange_recycler_layout, viewGroup, false);
+        MyViewHolder vh = new MyViewHolder(constraintLayout);
+        return vh;
+    }
+
+    // a binder that takes the view holder and inserts data in it
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        try {
+            JSONObject obj = mDataset.getJSONObject(i);
+            String personName = obj.getString("name");
+            Double personExchange = obj.getDouble("exchange");
+            myViewHolder.personName.setText(personName);
+            myViewHolder.personExchange.setText(personExchange.toString());
+            int green = ContextCompat.getColor(myViewHolder.personName.getContext(), R.color.colorPrimaryDark);
+            int pink = ContextCompat.getColor(myViewHolder.personName.getContext(), R.color.colorAccent);
+            if(personExchange >= 0){
+                myViewHolder.personExchange.setTextColor(green);
+            } else {
+                myViewHolder.personExchange.setTextColor(pink);
+            }
+        } catch (JSONException jsonException){
+            // TODO: Proper exception handling here
+        }
+    }
+}
--- app/src/main/java/com/example/moazin/easylend/ExchangeFragment.java
+++ app/src/main/java/com/example/moazin/easylend/ExchangeFragment.java
@@ -3,9 +3,17 @@ package com.example.moazin.easylend;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.JsonReader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -18,14 +26,31 @@ public class ExchangeFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exchange, container, false);
+        View view = inflater.inflate(R.layout.fragment_exchange, container, false);
+        // setting up data for populating recycler view (at least for testing)
+        JSONObject moazin = new JSONObject();
+        JSONObject wahab = new JSONObject();
+        try {
+            moazin.put("name", "Moazin Khatri");
+            moazin.put("exchange", 50);
+            wahab.put("name", "Abdul Wahab");
+            wahab.put("exchange", -100);
+        } catch(JSONException jsonException){
+            // TODO: Properly handle this shit
+        }
+        JSONArray array = new JSONArray();
+        array.put(moazin);
+        array.put(wahab);
+        // setting up the Recycler View
+        RecyclerView recyclerView = view.findViewById(R.id.exchange_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ExchangeAdapter exchangeAdapter = new ExchangeAdapter(array);
+        recyclerView.setAdapter(exchangeAdapter);
+        return view;
     }
 
 }

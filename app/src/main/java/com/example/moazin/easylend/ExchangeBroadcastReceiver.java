f907d9f4c962781d60184c1b0f0610fd2041e2b6
Moazin Khatri <moazinkhatri@gmail.com>
2018-12-08 14:00:27 +0500

    Doing refreshing and loading without polling, just on resumes and pull to refresh events

--- app/src/main/java/com/example/moazin/easylend/ExchangeBroadcastReceiver.java
+++ app/src/main/java/com/example/moazin/easylend/ExchangeBroadcastReceiver.java
@@ -3,8 +3,13 @@ package com.example.moazin.easylend;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.renderscript.ScriptGroup;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,25 +17,27 @@ import org.json.JSONException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ExchangeBroadcastReceiver extends BroadcastReceiver {
     private ExchangeAdapter exchangeAdapter;
+    private ConstraintLayout exchange_progressbar_constraint;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
-    public ExchangeBroadcastReceiver(ExchangeAdapter adapter){
+    public ExchangeBroadcastReceiver(ExchangeAdapter adapter, ConstraintLayout constraintLayout, SwipeRefreshLayout swipeLayout){
         exchangeAdapter = adapter;
+        exchange_progressbar_constraint = constraintLayout;
+        swipeRefreshLayout = swipeLayout;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Hey dude just received a signal", Toast.LENGTH_LONG).show();
+        Log.d("RECEIVE BROADCAST: ", "Received a broadcast");
         FileInputStream file = null;
         InputStreamReader inputStreamReader = null;
         BufferedReader bufferedReader = null;
         try {
-            // TODO: DO proper handling here
             file = new FileInputStream(new File(context.getCacheDir(), "exchange_data.json"));
             inputStreamReader = new InputStreamReader(file);
             bufferedReader = new BufferedReader(inputStreamReader);
@@ -42,7 +49,13 @@ public class ExchangeBroadcastReceiver extends BroadcastReceiver {
             String ret = stringBuilder.toString();
             JSONArray array = new JSONArray(ret);
             exchangeAdapter.setData(array);
-            // setting up the Recycler View
+            if(exchange_progressbar_constraint.getVisibility() == View.VISIBLE){
+                exchange_progressbar_constraint.setVisibility(View.GONE);
+            }
+            if(swipeRefreshLayout.getVisibility() == View.GONE){
+                swipeRefreshLayout.setVisibility(View.VISIBLE);
+            }
+            swipeRefreshLayout.setRefreshing(false);
         } catch (IOException|JSONException ex) {
             Toast.makeText(context, ex.toString(), Toast.LENGTH_LONG).show();
         } finally {
@@ -55,4 +68,6 @@ public class ExchangeBroadcastReceiver extends BroadcastReceiver {
             }
         }
     }
+
+
 }
--- app/src/main/java/com/example/moazin/easylend/ExchangeFragment.java
+++ app/src/main/java/com/example/moazin/easylend/ExchangeFragment.java
@@ -1,11 +1,15 @@
 package com.example.moazin.easylend;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
@@ -15,24 +19,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ExchangeFragment extends Fragment {
+
+
     ExchangeAdapter exchangeAdapter;
     Thread synchronizer_thread;
+    ConstraintLayout progress_bar_constraint;
+    SwipeRefreshLayout swipeRefreshLayout;
+    private RequestQueue queue;
+
     public ExchangeFragment() {
         // Required empty public constructor
     }
@@ -41,23 +64,88 @@ public class ExchangeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_exchange, container, false);
-        // register the broadcast receiver
-        IntentFilter intentFilter = new IntentFilter("moazin.khatri");
-        // Inflate the layout for this fragment
         exchangeAdapter = new ExchangeAdapter();
+        progress_bar_constraint = view.findViewById(R.id.progress_exchange_constraintlayout);
+        swipeRefreshLayout = view.findViewById(R.id.exchange_swiperefresh_layout);
+
+        // register the broadcast receiver
+        IntentFilter intentFilter = new IntentFilter(getString(R.string.exchange_data_ready));
+        LocalBroadcastManager.getInstance(getContext())
+                .registerReceiver(new ExchangeBroadcastReceiver
+                        (exchangeAdapter,
+                         progress_bar_constraint, swipeRefreshLayout), intentFilter);
+
+
+        // setup loading methods
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                RefreshData();
+            }
+        });
+
         RecyclerView recyclerView = view.findViewById(R.id.exchange_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(exchangeAdapter);
         recyclerView.setHasFixedSize(true);
-        LocalBroadcastManager.getInstance(getContext())
-                .registerReceiver(new ExchangeBroadcastReceiver(exchangeAdapter), intentFilter);
-        Intent do_once=  new Intent();
-        do_once.setAction("moazin.khatri");
+        Intent do_once = new Intent();
+        do_once.setAction(getString(R.string.exchange_data_ready));
         LocalBroadcastManager.getInstance(getContext()).sendBroadcast(do_once);
-
-        synchronizer_thread = new Thread(new ExchangeSynchronizer(getContext()));
-        synchronizer_thread.start();
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        RefreshData();
+    }
+
+    public void RefreshData(){
+        String base_url = getString(R.string.base_url_emulator);
+        String url = "http://" + base_url + ":8000/transactions/myexchangewitheveryone";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        File directory = getContext().getCacheDir();
+                        File file = new File(directory, "exchange_data.json");
+                        try {
+                            FileWriter fileWriter = new FileWriter(file);
+                            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                            bufferedWriter.write(response.toString());
+                            bufferedWriter.close();
+                            fileWriter.close();
+                            Intent intent = new Intent();
+                            intent.setAction(getString(R.string.exchange_data_ready));
+                            LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
+                        } catch (IOException ioe) {
+                            // TODO: Proper error handling
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                SharedPreferences sharedPreferences = getContext().getSharedPreferences("user_info", Context.MODE_PRIVATE);
+                String token = sharedPreferences.getString("token", "no_token");
+                params.put("Authorization", "Token " + token);
+                return params;
+            }
+        };
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
+                2000,
+                0,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
+        queue = Volley.newRequestQueue(getContext());
+        queue.add(jsonArrayRequest);
     }
 
 }
